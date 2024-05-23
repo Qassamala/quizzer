2496f6bca5649ccadfecefbeedc55b5fb5a6224e
Abdi <abdigobena@hotmail.com>
2024-05-23 13:09:44 +0200

    Enabled css and some styling to buttons, divs. Also same heaer througout the app

--- frontend/quizzer/src/App.js
+++ frontend/quizzer/src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NewQuizPage from './pages/NewQuizPage';
 import LoginPage from './pages/LoginPage';
+import './css/App.css'; 
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/new-quiz" element={<NewQuizPage/>} />
--- frontend/quizzer/src/pages/HomePage.js
+++ frontend/quizzer/src/pages/HomePage.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Quiz App</h1>
+    <div className='general-div'>
       <Link to="/new-quiz">
-        <button>Start a New Quiz</button>
+        <button className='general-button'>New Quiz</button>
       </Link>
-      <Link to="/existing-quiz">
-        <button>Enter an Existing Quiz</button>
-      </Link>
+      <form>
+        <label>
+          Enter quiz code to join:
+          <input type="text" name="code" />
+        </label>
+        <Link to="/existing-quiz">
+        <input type="submit" value="Submit" />
+        </Link>
+      </form>
       <Link to="/login">
         <button>Login</button>
       </Link>
