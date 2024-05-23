6eb3777c92c222bd5f907ab9da17808b4769d1f3
Abdi <abdigobena@hotmail.com>
2024-05-23 14:37:59 +0200

    Set input below label in form

--- frontend/quizzer/src/pages/HomePage.js
+++ frontend/quizzer/src/pages/HomePage.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const code = e.target.elements.code.value;
+    // Add logic to verify the code or any other form submission logic here
+    if (code) {
+      navigate('/existing-quiz'); // Navigate to the existing quiz page
+    }
+  };
+
   return (
     <div className='general-div'>
+      <form onSubmit={handleSubmit}>
+        <div className='main-form'>
+          <label className='form-label'>
+            Enter quiz code to join:
+            <input type="text" name="code" />
+          </label>
+          <input className='form-input' type="submit" value="Submit" />
+        </div>
+      </form>
       <Link to="/new-quiz">
         <button className='general-button'>New Quiz</button>
       </Link>
-      <form>
-        <label>
-          Enter quiz code to join:
-          <input type="text" name="code" />
-        </label>
-        <Link to="/existing-quiz">
-        <input type="submit" value="Submit" />
-        </Link>
-      </form>
       <Link to="/login">
-        <button>Login</button>
+        <button className='general-button'>Login</button>
       </Link>
     </div>
   );
 };
 
 export default HomePage;
+
 
